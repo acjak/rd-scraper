4f816c3021bad204b24f5650ba08afef3ef4a540
andcj <sterrior@gmail.com>
2021-01-27 20:48:38 +0100

    Final changes

--- scraper.py
+++ scraper.py
@@ -1,11 +1,15 @@
 #!/usr/local/bin/python3.8
 
 import requests
+from time import sleep
 from bs4 import BeautifulSoup
 
-r = requests.get('https://rd.dk/da-dk/privat/koeb-bolig/Kurser-og-renter/Pages/aktuelle-kurser.aspx?valutakode=DKK')
-soup = BeautifulSoup(r.content, 'html.parser')
+while True:
+    r = requests.get('https://rd.dk/da-dk/privat/koeb-bolig/Kurser-og-renter/Pages/aktuelle-kurser.aspx?valutakode=DKK')
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-t1 = soup.select('#ctl00_m_g_88a3ff31_a0da_4002_9fec_f54a3232962b_grdData1_\#_1004')[0]
+    t1 = soup.select('#ctl00_m_g_88a3ff31_a0da_4002_9fec_f54a3232962b_grdData1_\#_1004')[0]
 
-print(t1.select(".gs-gridLinkColCell.gs-gridCell2 a")[1].text)
+    print(t1.select(".gs-gridLinkColCell.gs-gridCell2 a")[1].text)
+
+    sleep(10)
